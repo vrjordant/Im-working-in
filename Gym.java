fab14cfcb095604178866c1173783f7b86c6d5d6
JHobbie <jaredhobbie1@gmail.com>
2018-09-22 13:40:49 -0400

    JORDANS TURN

--- Gym.java
+++ Gym.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class Gym implements Runnable{
 	private Map<ApparatusType,Semaphore> apparati;
 	private Map<WeightPlateSize,Semaphore> weights;
+	private Semaphore weightMutex;
 	public Gym() {
 		apparati = new HashMap<ApparatusType, Semaphore>();
 		for(ApparatusType apparatus: ApparatusType.values()) {
@@ -19,7 +21,9 @@ public class Gym implements Runnable{
 		weights.put(WeightPlateSize.values()[0], new Semaphore(110));
 		weights.put(WeightPlateSize.values()[1], new Semaphore(90));
 		weights.put(WeightPlateSize.values()[2], new Semaphore(75));
+		weightMutex = new Semaphore(1);
 	}
+	
 	// Defines a bunch of data structures
 	private static final int GYM_SIZE = 30;
 	private static final int GYM_REGISTERED_CLIENTS = 10000;
@@ -43,15 +47,21 @@ public class Gym implements Runnable{
 	// Number of plates for each exercise should be between 0 and 10 per weight size
 	// Make sure there's at least one plate
 	// Simulation should print when a person starts exercising, including apparatus number and the weight plates, and when it finishes
-
-	// Does the gym need a constructor?
-
+	
 	/**
 	 * In charge of the simulation itself
 	 * Threads for clients should be spawned using a thread pool
 	 */
 	public void run() {
 		// TODO: Implement me!
+		executor = Executors.newFixedThreadPool(GYM_SIZE); 
+		executor.execute(new Runnable () {
+			public void run() {
+				Client newGuy = Client.generateRandom(id)
+				System.out.println("I am an asynchronous task!");
+			}
+		});
+		executor.shutdown();
 	}
 
 }
